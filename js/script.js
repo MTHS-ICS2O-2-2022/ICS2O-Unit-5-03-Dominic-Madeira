023e27ed8f75af76c65c0e57e4d8c0de769be5e9
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-05-01 16:50:39 +0000

    first edit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Dominic M. All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Dominic M.
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+
+  const age = parseInt(document.getElementById('age-entered').value)
+
+  if (age >= 18) {
+
+    document.getElementById('answer').innerHTML = 'You can see an R rated movie alone.'
+
+  } else if (age >= 13) {
+
+    document.getElementById('answer').innerHTML = 'You can see a PG-13 movie alone.'
+
+  } else {
+      
+      document.getElementById('answer').innerHTML = 'You can see a G rated movie alone.'
+  }
+}
